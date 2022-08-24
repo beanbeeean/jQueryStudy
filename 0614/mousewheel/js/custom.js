cbf749dc59b07d2e22a8768f2d0e41883c29585c
beeean <beanbeeean@naver.com>
2022-08-24 21:52:48 +0900

    jQuery niceScroll

--- 0614/mousewheel/js/custom.js
+++ 0614/mousewheel/js/custom.js
@@ -1,22 +1,20 @@
 $(document).ready(function () {
+  var ht = $(window).height();
+  $("section").height(ht);
 
-    var ht = $(window).height();
-    $('section').height(ht);
+  // $(window).on('resize', function () {
+  //     var ht = $(window).height();
+  //     $('section').height(ht);
+  // });
 
-    $(window).on('resize', function () {
-        var ht = $(window).height();
-        $('section').height(ht);
-    });
-
-    $('section').on('mousewheel', function (event, delta) {
-        if (delta > 0) {
-            var prev = $(this).prev().offset().top;
-            $('html, body').stop().animate({ 'scrollTop': prev }, 1000, 'easeOutExpo');
-        } else if (delta < 0) {
-            var next = $(this).next().offset().top;
-            $('html, body').stop().animate({ 'scrollTop': next }, 1000, 'easeOutExpo');
-        }
-    });
-
+  $("section").on("mousewheel", function (event, delta) {
+    console.log(delta);
+    if (delta > 0) {
+      var prev = $(this).prev().offset().top;
+      $("html, body").stop().animate({ scrollTop: prev }, 1000, "easeOutExpo");
+    } else if (delta < 0) {
+      var next = $(this).next().offset().top;
+      $("html, body").stop().animate({ scrollTop: next }, 1000, "easeOutExpo");
+    }
+  });
 });
-
